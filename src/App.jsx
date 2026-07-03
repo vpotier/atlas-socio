c0f7c0886280f9c52ee926563c904df6a6cf7842
vpotier <victor.potier@univ-eiffel.fr>
2026-07-03 17:41:17 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,3 @@
+import Graph from './components/Graph';
+import data from './data/ats_graph.json';
+export default function App(){return <div><h1>Atlas de la théorie sociologique</h1><Graph data={data}/></div>}
---
+++ src/components/Graph.jsx
@@ -0,0 +1,6 @@
+export default function Graph({data}){
+return <div style={{padding:20}}>
+<p>Prototype initial.</p>
+<pre>{JSON.stringify(data,null,2)}</pre>
+</div>
+}

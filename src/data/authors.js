085e8fec01864fdb5ed8d2d56e7f65f7b6496d79
vpotier <victor.potier@univ-eiffel.fr>
2026-07-06 11:30:45 +0200

    Create authors.js

---
+++ src/data/authors.js
@@ -0,0 +1,43 @@
+export const authors = [
+  {
+    id: "durkheim",
+    name: "Durkheim",
+    school: "durkheimiens",
+    x: 165,
+    y: 185,
+    color: "#2E6DB4",
+    concepts: [
+      "fait social",
+      "solidarité",
+      "anomie"
+    ]
+  },
+
+  {
+    id: "weber",
+    name: "Weber",
+    school: "weberiens",
+    x: 720,
+    y: 180,
+    color: "#B53B3B",
+    concepts: [
+      "action sociale",
+      "idéal-type",
+      "domination"
+    ]
+  },
+
+  {
+    id: "bourdieu",
+    name: "Bourdieu",
+    school: "bourdieusiens",
+    x: 420,
+    y: 405,
+    color: "#86B53F",
+    concepts: [
+      "habitus",
+      "champ",
+      "capital"
+    ]
+  }
+];

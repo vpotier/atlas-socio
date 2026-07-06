5fee3c82fb179e1a74107788a93a05eee8a2c627
vpotier <victor.potier@univ-eiffel.fr>
2026-07-06 14:40:25 +0200

    Create culsters.js

---
+++ src/engine/culsters.js
@@ -0,0 +1,25 @@
+export const clusters = {
+  durkheimien: {
+    id: "durkheimien",
+    label: "Durkheimien",
+    center: { x: 160, y: 200 },
+    color: "#7fb3e8",
+    authors: ["durkheim"],
+  },
+
+  weberien: {
+    id: "weberien",
+    label: "Wébérien",
+    center: { x: 720, y: 200 },
+    color: "#d88b8b",
+    authors: ["weber"],
+  },
+
+  bourdieusien: {
+    id: "bourdieusien",
+    label: "Bourdieusien",
+    center: { x: 420, y: 420 },
+    color: "#b8d87d",
+    authors: ["bourdieu"],
+  },
+};

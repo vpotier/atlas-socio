b50cd102bfb6c94547a6adab17f612b4a71a0ecb
vpotier <victor.potier@univ-eiffel.fr>
2026-07-06 14:33:22 +0200

    Update concepts.js

--- src/data/concepts.js
+++ src/data/concepts.js
@@ -1,1 +1,30 @@
-
+export const concepts = [
+  {
+    id: "habitus",
+    label: "Habitus",
+    authors: ["bourdieu"],
+    x: 420,
+    y: 470,
+  },
+  {
+    id: "champ",
+    label: "Champ",
+    authors: ["bourdieu"],
+    x: 460,
+    y: 420,
+  },
+  {
+    id: "anomie",
+    label: "Anomie",
+    authors: ["durkheim"],
+    x: 140,
+    y: 240,
+  },
+  {
+    id: "action-sociale",
+    label: "Action sociale",
+    authors: ["weber"],
+    x: 720,
+    y: 260,
+  },
+];

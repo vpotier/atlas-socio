ce19cb5e563214673b0fda2ce53bc89abee87338
vpotier <victor.potier@univ-eiffel.fr>
2026-07-06 15:50:24 +0200

    Update Blends.jsx

--- src/components/Blends.jsx
+++ src/components/Blends.jsx
@@ -1,33 +1,3 @@
-import { clusters } from "../engine/clusters";
-
 export default function Blends() {
-  const list = Object.values(clusters);
-
-  return (
-    <>
-      {list.map((cluster) =>
-        (cluster.blend || []).map((targetId) => {
-          const target = list.find((c) => c.id === targetId);
-          if (!target) return null;
-
-          return (
-            <path
-              key={`${cluster.id}-${target.id}`}
-              d={`
-                M ${cluster.center.x} ${cluster.center.y}
-                Q ${(cluster.center.x + target.center.x) / 2}
-                  ${(cluster.center.y + target.center.y) / 2 - 80}
-                  ${target.center.x}
-                  ${target.center.y}
-              `}
-              stroke={cluster.color}
-              strokeWidth="2"
-              fill="none"
-              opacity="0.25"
-            />
-          );
-        })
-      )}
-    </>
-  );
+  return null;
 }

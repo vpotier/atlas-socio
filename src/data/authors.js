29232b21bd6fa0a3a1a84bb7cb660bafa04c6e61
vpotier <victor.potier@univ-eiffel.fr>
2026-07-06 15:18:13 +0200

    Update authors.js

--- src/data/authors.js
+++ src/data/authors.js
@@ -1,43 +1,108 @@
 export const authors = [
   {
     id: "durkheim",
-    name: "Durkheim",
-    school: "durkheimiens",
+    name: "Émile Durkheim",
+    school: "École durkheimienne",
     x: 165,
     y: 185,
     color: "#2E6DB4",
+
     concepts: [
-      "fait social",
-      "solidarité",
-      "anomie"
-    ]
+      "Fait social",
+      "Solidarité",
+      "Anomie",
+    ],
+
+    works: [
+      "De la division du travail social",
+      "Les Règles de la méthode sociologique",
+      "Le Suicide",
+      "Les Formes élémentaires de la vie religieuse",
+    ],
+
+    influences: [
+      "Comte",
+      "Saint-Simon",
+    ],
+
+    heirs: [
+      "Mauss",
+      "Halbwachs",
+      "Bourdieu",
+    ],
+
+    summary:
+      "Fondateur de la sociologie française. Il explique les phénomènes sociaux par des faits sociaux extérieurs aux individus.",
   },
 
   {
     id: "weber",
-    name: "Weber",
-    school: "weberiens",
+    name: "Max Weber",
+    school: "Sociologie compréhensive",
     x: 720,
     y: 180,
     color: "#B53B3B",
+
     concepts: [
-      "action sociale",
-      "idéal-type",
-      "domination"
-    ]
+      "Action sociale",
+      "Domination",
+      "Idéal-type",
+    ],
+
+    works: [
+      "Économie et société",
+      "Le Savant et le Politique",
+      "L'Éthique protestante",
+    ],
+
+    influences: [
+      "Nietzsche",
+      "Rickert",
+    ],
+
+    heirs: [
+      "Aron",
+      "Boudon",
+    ],
+
+    summary:
+      "Fondateur de la sociologie compréhensive. Il cherche à comprendre le sens que les individus donnent à leurs actions.",
   },
 
   {
     id: "bourdieu",
-    name: "Bourdieu",
-    school: "bourdieusiens",
+    name: "Pierre Bourdieu",
+    school: "Structuralisme constructiviste",
     x: 420,
     y: 405,
     color: "#86B53F",
+
     concepts: [
-      "habitus",
-      "champ",
-      "capital"
-    ]
-  }
+      "Habitus",
+      "Champ",
+      "Capital",
+    ],
+
+    works: [
+      "La Distinction",
+      "Le Sens pratique",
+      "Les Héritiers",
+      "Méditations pascaliennes",
+    ],
+
+    influences: [
+      "Durkheim",
+      "Weber",
+      "Marx",
+    ],
+
+    heirs: [
+      "Lahire",
+      "Boltanski",
+      "Wacquant",
+    ],
+
+    summary:
+      "Il articule structures sociales et pratiques individuelles grâce aux notions d'habitus, de champ et de capitaux.",
+  },
 ];

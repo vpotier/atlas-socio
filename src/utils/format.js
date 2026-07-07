fc5d06f93efa2509388effb9883d17705e3feadd
vpotier <victor.potier@univ-eiffel.fr>
2026-07-07 12:06:35 +0200

    Create format.js

---
+++ src/utils/format.js
@@ -0,0 +1,41 @@
+import { authors } from "../data/authors";
+import { people } from "../data/people";
+
+function formatWithDates(fullName, birthYear, deathYear) {
+  if (!birthYear) return fullName;
+
+  if (!deathYear) {
+    return `${fullName} (né en ${birthYear})`;
+  }
+
+  return `${fullName} (${birthYear} - ${deathYear})`;
+}
+
+export function formatPerson(shortName) {
+  const author = authors.find((a) => a.name.endsWith(shortName));
+
+  if (author) {
+    return formatWithDates(
+      author.name,
+      author.birthYear,
+      author.deathYear
+    );
+  }
+
+  const person = people[shortName];
+
+  if (person) {
+    return formatWithDates(
+      person.fullName,
+      person.birthYear,
+      person.deathYear
+    );
+  }
+
+  return shortName;
+}
+
+export function workSearchUrl(title, authorName) {
+  const query = encodeURIComponent(`${title} ${authorName}`);
+  return `https://fr.wikipedia.org/w/index.php?search=${query}`;
+}

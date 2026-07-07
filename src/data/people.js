c7f45e96a4acbc266e6b22e3b5ac0b38a79ba9ce
vpotier <victor.potier@univ-eiffel.fr>
2026-07-07 12:05:29 +0200

    Create people.js

---
+++ src/data/people.js
@@ -0,0 +1,26 @@
+export const people = {
+  Comte: { fullName: "Auguste Comte", birthYear: 1798, deathYear: 1857 },
+  "Saint-Simon": {
+    fullName: "Henri de Saint-Simon",
+    birthYear: 1760,
+    deathYear: 1825,
+  },
+  Mauss: { fullName: "Marcel Mauss", birthYear: 1872, deathYear: 1950 },
+  Halbwachs: {
+    fullName: "Maurice Halbwachs",
+    birthYear: 1877,
+    deathYear: 1945,
+  },
+  Nietzsche: {
+    fullName: "Friedrich Nietzsche",
+    birthYear: 1844,
+    deathYear: 1900,
+  },
+  Rickert: { fullName: "Heinrich Rickert", birthYear: 1863, deathYear: 1936 },
+  Aron: { fullName: "Raymond Aron", birthYear: 1905, deathYear: 1983 },
+  Boudon: { fullName: "Raymond Boudon", birthYear: 1934, deathYear: 2013 },
+  Marx: { fullName: "Karl Marx", birthYear: 1818, deathYear: 1883 },
+  Lahire: { fullName: "Bernard Lahire", birthYear: 1963, deathYear: null },
+  Boltanski: { fullName: "Luc Boltanski", birthYear: 1940, deathYear: null },
+  Wacquant: { fullName: "Loïc Wacquant", birthYear: 1960, deathYear: null },
+};

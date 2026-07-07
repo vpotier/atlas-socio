c9963e38d1873d7b93fb5cfa6a508b271cf365f7
vpotier <victor.potier@univ-eiffel.fr>
2026-07-07 11:45:13 +0200

    Update Authors.jsx

--- src/components/Authors.jsx
+++ src/components/Authors.jsx
@@ -16,6 +16,7 @@ export default function Authors({
         return (
           <g
             key={author.id}
+            id={author.id}
             style={{
               cursor: "pointer",
               transition: "all .25s",

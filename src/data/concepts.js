6e8d8e9e67673f92d7d7bb96652fef621cfa73d3
vpotier <victor.potier@univ-eiffel.fr>
2026-07-08 09:49:19 +0200

    Update concepts.js

--- src/data/concepts.js
+++ src/data/concepts.js
@@ -31,8 +31,35 @@ export const concepts = [
     label: "Action sociale",
     definition:
       "Comportement humain auquel l'acteur attribue un sens subjectif et qui prend en compte le comportement d'autrui, orientant ainsi sa propre conduite.",
-    authors: ["weber"],
+    authors: ["weber", "talcott-parsons"],
     x: 720,
     y: 260,
+  },
+  {
+    id: "modele-agil",
+    label: "Modèle AGIL",
+    definition:
+      "Grille d'analyse systémique postulant que toute société doit remplir quatre fonctions fondamentales pour se maintenir : l'adaptation aux ressources (A), la poursuite des objectifs (G), l'intégration des membres (I) et la maintenance des modèles culturels latents (L).",
+    authors: ["talcott-parsons"],
+    x: 950,
+    y: 400,
+  },
+  {
+    id: "fonction-manifeste-et-latente",
+    label: "Fonction manifeste et latente",
+    definition:
+      "Distinction théorique isolant les conséquences objectives d'une action sociale consciemment recherchées et reconnues par les participants (manifestes) de ses conséquences non intentionnelles et non perçues (latentes).",
+    authors: ["robert-k-merton"],
+    x: 860,
+    y: 650,
+  },
+  {
+    id: "equivalent-fonctionnel",
+    label: "Équivalent fonctionnel",
+    definition:
+      "Concept stipulant qu'une seule et même fonction sociale peut être remplie par des éléments structurels, des pratiques ou des institutions différentes et interchangeables au sein d'une société.",
+    authors: ["robert-k-merton"],
+    x: 940,
+    y: 650,
   },
 ];

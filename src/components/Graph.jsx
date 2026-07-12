8e135682a199860b67f8de601271ee610502dc92
vpotier <victor.potier@univ-eiffel.fr>
2026-07-12 09:46:10 +0200

    Update Graph.jsx

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -198,12 +198,13 @@ export default function Graph({
   };
 
   return (
-    <TransformWrapper
+   <TransformWrapper
       ref={transformRef}
       initialScale={0.55}
       minScale={0.2}
       maxScale={3}
       centerOnInit
+      centerZoomedOut
     >
       <TransformComponent>
         <svg

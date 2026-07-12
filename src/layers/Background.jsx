270cebb8fcae1488befbb0b9d163ab0f06aef654
vpotier <victor.potier@univ-eiffel.fr>
2026-07-12 09:38:52 +0200

    Update Background.jsx

--- src/layers/Background.jsx
+++ src/layers/Background.jsx
@@ -1,35 +1,39 @@
-export default function Background() {
+export default function Background({ width, height }) {
+  const step = 50;
+  const vLines = Math.ceil(width / step) + 1;
+  const hLines = Math.ceil(height / step) + 1;
+
   return (
     <g>
       {/* Fond */}
       <rect
         x="0"
         y="0"
-        width="950"
-        height="620"
+        width={width}
+        height={height}
         fill="#f8f8f5"
       />
 
       {/* Grille légère */}
-      {[...Array(19)].map((_, i) => (
+      {[...Array(vLines)].map((_, i) => (
         <line
           key={"v" + i}
-          x1={i * 50}
+          x1={i * step}
           y1="0"
-          x2={i * 50}
-          y2="620"
+          x2={i * step}
+          y2={height}
           stroke="#ece9e2"
           strokeWidth="1"
         />
       ))}
 
-      {[...Array(13)].map((_, i) => (
+      {[...Array(hLines)].map((_, i) => (
         <line
           key={"h" + i}
           x1="0"
-          y1={i * 50}
-          x2="950"
-          y2={i * 50}
+          y1={i * step}
+          x2={width}
+          y2={i * step}
           stroke="#ece9e2"
           strokeWidth="1"
         />

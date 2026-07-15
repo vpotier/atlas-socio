3ecd001cafd81c0e6f2e70740755b63e41e15bc7
vpotier <victor.potier@univ-eiffel.fr>
2026-07-15 09:25:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -499,7 +499,7 @@ export default function App() {
         }}
       >
         Par Victor Potier —{" "}
-        
+        <a
           href="mailto:victor.potier@univ-eiffel.fr"
           style={{ color: "var(--color-tardis)" }}
         >

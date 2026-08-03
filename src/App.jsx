bc220c5ed86aa6d9ab1c5109e5fe2a852e5e6664
vpotier <victor.potier@univ-eiffel.fr>
2026-08-03 17:00:48 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 import Graph from "./components/Graph";
 import SearchBar from "./components/SearchBar";
